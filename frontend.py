29b677089f920f6f297b3ed104477bf55e4d0cf7
Reid Whitson <reidwhitson7@gmail.com>
2019-10-20 12:43:05 -0700

    First push

--- frontend.py
+++ frontend.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello from guadr!</h1/"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
