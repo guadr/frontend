321f5796410332ab130a74a2fc4b7a451790fe85
papahance98 <khance@zagmail.gonzaga.edu>
2019-10-23 20:26:25 -0700

    map takes user input and updates marker and view, added styling

--- frontend.py
+++ frontend.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 
 current_percentage = 30
 remaining_time = 100
-latitude_robot = 47.667191
-longitude_robot = -117.402382
-latitude_destination = 47.666532
-longitude_destination = -117.400659
-lat_offset = 0.0063
+latitude_robot = 47.667560
+longitude_robot = -117.401629
+latitude_destination = 47.666867
+longitude_destination = -117.401701
+lat_offset = 0.00633
 long_offset = 0.00755
-hc_location_url = "https://www.openstreetmap.org/export/embed.html?bbox=-117.4111032485962%2C47.66108082037928%2C-117.39591121673585%2C47.67368149482369&amp;layer=mapnik&amp;marker=47.66738153785244%2C-117.40350723266602"
-location_url = "https://www.openstreetmap.org/export/embed.html?bbox=" + (str(longitude_destination - 0.00755)) + "%2C" + (str(latitude_destination - 0.0063)) + "%2C" + (str(longitude_destination + 0.00755)) + "%2C" + (str(latitude_destination - 0.0063)) + "&amp;layer=mapnik&amp;marker=" + (str(latitude_destination)) + "%2C" + (str(longitude_destination))
+hc_location_url = "https://www.openstreetmap.org/?mlat=47.66753&mlon=-117.40291#map=18/47.66753/-117.40291"
+location_url = "https://www.openstreetmap.org/export/embed.html?bbox=" + (str(longitude_destination - long_offset)) + "%2C" + (str(latitude_destination - lat_offset)) + "%2C" + (str(longitude_destination + long_offset)) + "%2C" + (str(latitude_destination + lat_offset)) + "&layer=mapnik&marker=" + (str(latitude_destination)) + "%2C" + (str(longitude_destination))
 
 food_items = ["Sandwich","Soda","Candy","Trail Mix","Beef Jerky","Muffin"]
 delivery_locations = ["Foley Library", "Hemmingson NW Corner", "Herak NE Corner", "Crosby North Entrance"]
@@ -21,7 +21,7 @@ longitudes_list = [longitude_robot, longitude_destination]
 
 @app.route("/")
 def hello():
-    return render_template("home.html", title="GUADR Mockup" , foods=food_items, locations=delivery_locations, cur_per=current_percentage, rem_time=remaining_time, loc_url=hc_location_url)
+    return render_template("home.html", title="GUADR Mockup" , foods=food_items, locations=delivery_locations, cur_per=current_percentage, rem_time=remaining_time, loc_url=location_url)
 
 '''def convertLatituteLongitudeToUrl(latitude, longitude){
     return "https://www.openstreetmap.org/export/embed.html?bbox="
