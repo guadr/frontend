240bdc10772f5263553d9163b735d6516160ce96
Reid Whitson <reidwhitson7@gmail.com>
2019-11-16 13:40:27 -0800

    added authentication and command line arguments for testing file

--- frontend.py
+++ frontend.py
@@ -6,16 +6,16 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash
 
 
-'''
+"""
 authorization using https://github.com/miguelgrinberg/Flask-HTTPAuth
-'''
+"""
 
 
-DATABASE = '../instance/GUADR.db' 
+DATABASE = "../instance/GUADR.db"
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-#Set Variables
+# Set Variables
 current_percentage = 30
 remaining_time = 100
 latitude_robot = 47.667560
@@ -26,150 +26,216 @@ lat_offset = 0.000633
 long_offset = 0.000755
 default_layer = "hot"
 hc_location_url = "https://www.openstreetmap.org/?mlat=47.66753&mlon=-117.40291#map=18/47.66753/-117.40291"
-location_url = "https://www.openstreetmap.org/export/embed.html?bbox=" + (str(longitude_destination - long_offset)) + "%2C" + (str(latitude_destination - lat_offset)) + "%2C" + (str(longitude_destination + long_offset)) + "%2C" + (str(latitude_destination + lat_offset)) + "&layer=" + default_layer + "&marker=" + (str(latitude_destination)) + "%2C" + (str(longitude_destination))
-food_items = ["Sandwich","Soda","Candy","Trail Mix","Beef Jerky","Muffin"]
-delivery_locations = ["Foley Library", "Hemmingson NW Corner", "Herak NE Corner", "Crosby North Entrance"]
+location_url = (
+    "https://www.openstreetmap.org/export/embed.html?bbox="
+    + (str(longitude_destination - long_offset))
+    + "%2C"
+    + (str(latitude_destination - lat_offset))
+    + "%2C"
+    + (str(longitude_destination + long_offset))
+    + "%2C"
+    + (str(latitude_destination + lat_offset))
+    + "&layer="
+    + default_layer
+    + "&marker="
+    + (str(latitude_destination))
+    + "%2C"
+    + (str(longitude_destination))
+)
+food_items = ["Sandwich", "Soda", "Candy", "Trail Mix", "Beef Jerky", "Muffin"]
+delivery_locations = [
+    "Foley Library",
+    "Hemmingson NW Corner",
+    "Herak NE Corner",
+    "Crosby North Entrance",
+]
 latitudes_list = [latitude_robot, latitude_destination]
 longitudes_list = [longitude_robot, longitude_destination]
 
+
 @auth.verify_password
 def verify_password(username, password):
-    base_user = query_db('select * from users', one=True)
-    if username == base_user['username'] and check_password_hash(base_user['password'],password):
+    base_user = query_db("select * from users", one=True)
+    if username == base_user["username"] and check_password_hash(
+        base_user["password"], password
+    ):
         return True
     return False
 
 
-#Home Route
+# Home Route
 @app.route("/")
 @auth.login_required
 def hello():
-    return render_template("home.html", title="GUADR Mockup" , foods=food_items, locations=delivery_locations, cur_per=current_percentage, rem_time=remaining_time, loc_url=location_url)
+    return render_template(
+        "home.html",
+        title="GUADR Mockup",
+        foods=food_items,
+        locations=delivery_locations,
+        cur_per=current_percentage,
+        rem_time=remaining_time,
+        loc_url=location_url,
+    )
+
 
 ##############
 #   API      #
 ##############
-@app.route('/location/api/delivery/robot_location', methods=['GET', 'POST'])
+@app.route("/location/api/delivery/robot_location", methods=["GET", "POST"])
+@auth.login_required
 def get_robot_location():
     if request.method == "GET":
-        '''
+        """
         If GET, get the latitude and longitude
         of the latest robot location 
-        '''
-        loc = query_db('''select latitude, longitude 
-                        from robot_location 
-                        where time = (select MAX(time) 
-                                             from robot_location)''')
+        """
+        loc = query_db(
+            """select latitude, longitude  
+                           from robot_location 
+                           where time = (select MAX(time)  
+                                         from robot_location) """
+        )
+
         return jsonify(loc)
+
     elif request.method == "POST":
-        '''
+        """
         If POST, get the latest delivery_id (the one that we are on, 
         since we are only handling one user), and insert the updated
         location
-        '''
-        robot_loc = [{
-                'latitude':  float(request.form['latitude']), 
-                'longitude': float(request.form['longitude'])
-        }]
+        """
+        robot_loc = [
+            {
+                "latitude": float(request.form["latitude"]),
+                "longitude": float(request.form["longitude"]),
+            }
+        ]
 
-        max_del = query_db('SELECT max(delivery_id) as id FROM delivery_location', one=True)
+        max_del = query_db(
+            "SELECT max(delivery_id) as id FROM delivery_location", one=True
+        )
 
-        insert_into_db('INSERT INTO robot_location (del_id, time, latitude, longitude) VALUES (?,?,?,?)', (max_del['id'], datetime.datetime.now(), float(request.form['latitude']), float(request.form['longitude'])))
+        insert_into_db(
+            "INSERT INTO robot_location (del_id, time, latitude, longitude) VALUES (?,?,?,?)",
+            (
+                max_del["id"],
+                datetime.datetime.now(),
+                float(request.form["latitude"]),
+                float(request.form["longitude"]),
+            ),
+        )
         return jsonify(robot_loc), 201
 
     else:
         abort(404)
 
-@app.route('/location/api/delivery/delivery_location', methods=['GET', 'POST'])
+
+@app.route("/location/api/delivery/delivery_location", methods=["GET", "POST"])
+@auth.login_required
 def get_delivery_location():
     if request.method == "GET":
-        '''
+        """
         If GET: get the latest
         long and lat to respond
-        '''
-        loc = query_db('''select latitude, longitude 
+        """
+        loc = query_db(
+            """select latitude, longitude 
                         from delivery_location 
                         where delivery_id = (select MAX(delivery_id) 
-                                                   from delivery_location)''')
+                                                   from delivery_location)"""
+        )
         return jsonify(loc)
 
     elif request.method == "POST":
-        '''
+        """
         IF post, get the given location
         and update the DB.
-        '''
-        delivery = [{
-                'latitude':  float(request.form['latitude']), 
-                'longitude': float(request.form['longitude'])
-        }]
+        """
+        delivery = [
+            {
+                "latitude": float(request.form["latitude"]),
+                "longitude": float(request.form["longitude"]),
+            }
+        ]
 
-        insert_into_db('INSERT INTO delivery_location ( latitude, longitude) VALUES (?, ?)', ( float(request.form['latitude']), float(request.form['longitude'])))
+        insert_into_db(
+            "INSERT INTO delivery_location ( latitude, longitude) VALUES (?, ?)",
+            (float(request.form["latitude"]), float(request.form["longitude"])),
+        )
         return jsonify(delivery), 201
     else:
         abort(404)
 
-'''
+
+"""
 Database: Code adopted from https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
-'''
+"""
+
+
 def get_db():
-    '''
+    """
     Connect to the dband 
     returns the db connection
-    '''
-    db = getattr(g, '_database', None)
+    """
+    db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-    db.row_factory = make_dicts #Make the module return dictionaries
+    db.row_factory = make_dicts  # Make the module return dictionaries
     return db
 
+
 def make_dicts(cursor, row):
-    '''
+    """
     Make a dictioanry with the given
     cursor and rows
-    '''
+    """
     return dict((cursor.description[idx][0], value) for idx, value in enumerate(row))
+
 
 @app.teardown_appcontext
 def close_connection(exception):
-    '''
+    """
     Close DB connection
-    '''
-    db = getattr(g, '_database', None)
+    """
+    db = getattr(g, "_database", None)
     if db is not None:
         db.close()
 
-def insert_into_db(query,args=()):
-    '''
+
+def insert_into_db(query, args=()):
+    """
     pass in query and arguments to 
     execute and commit changes
     on databse.
-    '''
+    """
     db = get_db()
     cur = db.execute(query, args)
     db.commit()
     cur.close()
 
+
 def query_db(query, args=(), one=False):
-    '''
+    """
     Replies a dictionary holding
     the response from query.
-    '''
+    """
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 def init_db():
-    '''
+    """
     Initialize the databse with the given schema.
-    '''
+    """
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource("schema.sql", mode="r") as f:
             db.cursor().executescript(f.read())
         db.commit()
 
 
 if __name__ == "__main__":
     init_db()
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
--- test_frontend.py
+++ test_frontend.py
@@ -3,39 +3,59 @@ import json
 import pytest
 import frontend
 from frontend import app
+import sys
 
-'''
+"""
 Test GET and POST functionality
 for both endpoints
-'''
-def test_delivery_POST():
-    x = requests.post('http://guadr.com/location/api/delivery/delivery_location', data={"latitude": 9, "longitude": 8})
-    json_request = json.loads(x.content)
-    assert (isinstance(json_request[0]['latitude'], float) and ( isinstance(json_request[0]['longitude'], float))) 
+"""
 
-def test_delivery_GET():
-    x = requests.get('http://guadr.com/location/api/delivery/delivery_location')
-    json_request = json.loads(x.content)
-    assert (isinstance(json_request[0]['latitude'], float) and ( isinstance(json_request[0]['longitude'], float))) 
 
-def test_robot_POST():
-    x = requests.post('http://guadr.com/location/api/delivery/robot_location', data={"latitude": 9, "longitude": 8})
-    json_request = json.loads(x.content)
-    assert (isinstance(json_request[0]['latitude'], float) and ( isinstance(json_request[0]['longitude'], float))) 
+def test_APIs(u, p):
+    session = requests.Session()
+    session.auth = (u, p)
 
-def test_robot_GET():
-    x = requests.get('http://guadr.com/location/api/delivery/robot_location')
+    x = session.post(
+        "http://0.0.0.0:5000/location/api/delivery/delivery_location",
+        data={"latitude": 9, "longitude": 8},
+    )
     json_request = json.loads(x.content)
-    assert (isinstance(json_request[0]['latitude'], float) and ( isinstance(json_request[0]['longitude'], float))) 
+    assert isinstance(json_request[0]["latitude"], float)
+    assert isinstance(json_request[0]["longitude"], float)
 
-'''
+    x = session.get("http://0.0.0.0:5000/location/api/delivery/delivery_location")
+    json_request = json.loads(x.content)
+    assert isinstance(json_request[0]["latitude"], float)
+    assert isinstance(json_request[0]["longitude"], float)
+
+    x = session.post(
+        "http://0.0.0.0:5000/location/api/delivery/robot_location",
+        data={"latitude": 9, "longitude": 8},
+    )
+    json_request = json.loads(x.content)
+    assert isinstance(json_request[0]["latitude"], float)
+    assert isinstance(json_request[0]["longitude"], float)
+
+    x = session.get("http://0.0.0.0:5000/location/api/delivery/robot_location")
+    json_request = json.loads(x.content)
+    assert isinstance(json_request[0]["latitude"], float)
+    assert isinstance(json_request[0]["longitude"], float)
+
+
+"""
 Test DB 
-'''
+"""
+
+
 def test_DB():
     with app.app_context():
         frontend.init_db()
-        frontend.insert_into_db('INSERT INTO robot_location (del_id, time, latitude, longitude) VALUES (0,0,0,0)')
-        result = frontend.query_db('select * from robot_location where del_id = 0')
-        result_fail = frontend.query_db('select * from robot_location where del_id = -1')
+        frontend.insert_into_db(
+            "INSERT INTO robot_location (del_id, time, latitude, longitude) VALUES (0,0,0,0)"
+        )
+        result = frontend.query_db("select * from robot_location where del_id = 0")
+        result_fail = frontend.query_db(
+            "select * from robot_location where del_id = -1"
+        )
     assert result != []
     assert result_fail == []
